8a859d74b0e1c9bd848946627aaeebafe005ecfd
jemsij3 <derartuaman8@gmail.com>
2026-07-11 22:05:17 +0300

    Delete src/component directory

--- src/component/Providers.tsx
+++
@@ -1,8 +0,0 @@
-"use compiled";
-"use client";
-
-import { SessionProvider } from "next-auth/react";
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
-}
